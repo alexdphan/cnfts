e948898c5cf1bd952c3ac7b20c08e741b89754fd
alexdphan <alexphan0515@gmail.com>
2022-12-11 22:33:43 -0500

    adding celeswasm dev

--- config/defaults.ts
+++ config/defaults.ts
@@ -6,9 +6,10 @@ import { AssetList, Asset } from '@chain-registry/types';
 
 export const chainName = process.env.NEXT_PUBLIC_CHAIN ?? 'celeswasm';
 
-export const chainassets: AssetList = assets.find(
-  (chain) => chain.chain_name === chainName
-) as AssetList;
-export const coin: Asset = chainassets.assets.find(
-  (asset) => asset.base === 'uwasm'
-) as Asset;
+// export const chainassets: AssetList = assets.find(
+//   (chain) => chain.chain_name === chainName
+// ) as AssetList;
+// export const coin: Asset = chainassets.assets.find(
+//   (asset) => asset.base === 'uwasm'
+// ) as Asset;
+// this comes from chain-registry
