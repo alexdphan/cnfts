6d3fc7b8d3a9f568388b7a0f2710f4677786b7f4
alexdphan <alexphan0515@gmail.com>
2022-12-14 01:13:56 -0500

    ts-codegen script progress

--- scripts/codegen.js
+++ scripts/codegen.js
@@ -1,8 +1,62 @@
-import codegen from '@cosmwasm/ts-codegen';
+// import codegen from '@cosmwasm/ts-codegen';
 // import { join } from 'path';
-// import { sync as rimraf } from 'rimraf';
+// import pkg from 'rimraf';
+// const { sync: rimraf } = pkg;
 
-codegen.default({
+// // const contractDir = join(__dirname, '../contracts/schemas/cnft721-base');
+// const contractDir = join(import.meta.url, '../contracts/schemas/cnft721-base');
+// // const outPath = join(__dirname, '../ts/src');
+// const outPath = join(import.meta.url, '../ts/src');
+// rimraf(outPath);
+
+// codegen.default({
+//   contracts: [
+//     {
+//       name: 'cnft721-base',
+//       dir: join(contractDir, 'cnft721-base'),
+//     },
+//     // {
+//     //   name: 'Minter',
+//     //   dir: './path/to/Minter/schema',
+//     // },
+//   ],
+//   outPath: './ts/src',
+
+//   // options are completely optional ;)
+//   options: {
+//     bundle: {
+//       bundleFile: 'index.ts',
+//       scope: 'contracts',
+//     },
+//     types: {
+//       enabled: true,
+//     },
+//     client: {
+//       enabled: true,
+//     },
+//     reactQuery: {
+//       enabled: true,
+//       optionalClient: true,
+//       version: 'v4',
+//       mutations: true,
+//       queryKeys: true,
+//       queryFactory: true,
+//     },
+//     recoil: {
+//       enabled: false,
+//     },
+//     messageComposer: {
+//       enabled: false,
+//     },
+//   },
+// }).then(() => {
+//   console.log('✨ all done!');
+// });
+
+import codegen from '@cosmwasm/ts-codegen';
+
+codegen
+  .default({
     contracts: [
       {
         name: 'cnft721-base',
